38bd0345d205f3304b691951abdc57ec3fdb9f08
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-08-15 11:28:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Trycatch
+{
+
+
+    class TryCatch
+    {
+
+        static void Main(string[] args)
+        {
+            bool IsItNumber = false;
+            while (!IsItNumber) { // we defined the bollan variable for the whille loop if it is dont true we turning loop
+                try
+                {
+                    Console.WriteLine("Please Enter the number:");
+                    int x = Convert.ToInt32(Console.ReadLine());
+                    int squared = x * x;
+                    Console.WriteLine($"Number's Square: {squared}"); // we print the number square
+                    IsItNumber = true;
+                }
+                catch (FormatException ex)// we used the formatexceprion settings toint32
+                {
+                    
+                    {
+                        Console.WriteLine("Invalid number please try again");// if it is wrong value
+
+                    } 
+                }
+            }
+        }
+    }
+}
